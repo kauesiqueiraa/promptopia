b4782d4f28f6fabb29918e4fc59b61fdd653a1ba
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-06-02 11:06:57 -0300

    :sparkles: feat: PromptCard and Feed

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,74 @@
-import React from 'react'
+'use client'
 
-export const Feed = () => {
-  return <div>Feed</div>
+import React, { ChangeEvent, useEffect, useState } from 'react'
+import { PromptCard } from './PromptCard'
+
+type Prompt = {
+  _id: string
+  creator: {
+    _id: string
+    username: string
+    email: string
+    image: string
+  }
+  prompt: string
+  tag: string
+  __v: number
+}
+
+const PromptCardList = ({
+  data = [],
+  handleTagClick,
+}: {
+  data: Prompt[]
+  handleTagClick: (tagName: string) => void
+}) => {
+  return (
+    <div className="prompt_layout mt-16">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default function Feed() {
+  const [searchText, setSearchText] = useState('')
+  const [post, setPost] = useState([])
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value)
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+
+      setPost(data)
+    }
+
+    fetchPosts()
+  }, [])
+
+  return (
+    <section className="feed">
+      <form className="flex-center relative w-full">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={post} handleTagClick={() => {}} />
+    </section>
+  )
 }
--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -1,5 +1,83 @@
-import React from 'react'
+'use client'
 
-export const PromptCard = () => {
-  return <div>PromptCard</div>
+import Image from 'next/image'
+import React, { FC, useState } from 'react'
+
+type Prompt = {
+  _id: string
+  creator: {
+    _id: string
+    username: string
+    email: string
+    image: string
+  }
+  prompt: string
+  tag: string
+  __v: number
+}
+
+type Props = {
+  post: Prompt
+  handleTagClick?: (tag: Prompt['tag']) => void
+  handleEdit?: (post: Prompt) => void
+  handleDelete?: (post: Prompt) => void
+}
+
+export const PromptCard: FC<Props> = ({
+  post,
+  handleTagClick,
+  handleEdit,
+  handleDelete,
+}) => {
+  const [copied, setCopied] = useState('')
+
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(''), 3000)
+  }
+  return (
+    <div className="prompt_card">
+      <div className="flex items-start justify-between gap-5">
+        <div className="flex flex-1 cursor-pointer items-center justify-start gap-3">
+          <Image
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {post.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {post.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn" onClick={handleCopy}>
+          <Image
+            src={
+              copied === post.prompt
+                ? '/assets/icons/tick.svg'
+                : '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+            alt="Copy"
+          />
+        </div>
+      </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="blue_gradient cursor-pointer font-inter text-sm"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  )
 }
